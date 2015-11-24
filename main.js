3047b6854b45d0a443131fd637a19880fc791f0d
syon <syon.xiii@gmail.com>
2015-11-24 23:56:47 +0900

    add form to change preview text

--- main.js
+++ main.js
@@ -3,10 +3,15 @@ var PreviewBox = React.createClass({
     this.setState({data: fonts});
   },
   getInitialState: function() {
-    return {data: []};
+    var dummy = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,";
+        dummy += " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    return {data: [], lorem: dummy};
   },
   componentDidMount: function() {
     var app = new InstalledFonts(this.loadPreviews);
+  },
+  handlePreviewSubmit: function(lorem) {
+    this.setState({lorem: lorem});
   },
   render: function() {
     return (
@@ -15,7 +20,8 @@ var PreviewBox = React.createClass({
           <PreviewMenu data={this.state.data} />
         </div>
         <div className="previewBox three-fourths column">
-          <PreviewList data={this.state.data} />
+          <PreviewForm lorem={this.state.lorem} onPreviewSubmit={this.handlePreviewSubmit} />
+          <PreviewList lorem={this.state.lorem} data={this.state.data} />
         </div>
       </div>
     );
@@ -37,11 +43,32 @@ var PreviewMenu = React.createClass({
   }
 });
 
+var PreviewForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var text = ReactDOM.findDOMNode(this.refs.text).value.trim();
+    if (!text) {
+      return;
+    }
+    this.props.onPreviewSubmit(text);
+    return;
+  },
+  render: function() {
+    return (
+      <form className="previewForm" onSubmit={this.handleSubmit}>
+        <textarea ref="text" defaultValue={this.props.lorem} className="input-block" placeholder="Preview text" />
+        <input type="submit" value="Update" className="btn" />
+      </form>
+    );
+  }
+});
+
 var PreviewList = React.createClass({
   render: function() {
+    var lorem = this.props.lorem;
     var previewNodes = this.props.data.map(function(preview, idx) {
       return (
-        <Preview fontFamily={preview.fontName} key={idx} />
+        <Preview fontFamily={preview.fontName} lorem={lorem} key={idx} />
       );
     });
     return (
@@ -59,8 +86,7 @@ var Preview = React.createClass({
       <div className="preview">
         <code className="fontname">{this.props.fontFamily}</code>
         <p className="preview" style={previewStyle}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          {this.props.lorem}
         </p>
       </div>
     );
